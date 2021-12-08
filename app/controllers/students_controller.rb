60e10a92913b705b056a26aa2a23d6dc234b7fa8
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-12-07 20:34:46 -0800

    added comments

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,17 @@
 class StudentsController < ApplicationController
    
-    def index
+    def index #3 In the index action, return JSON data representing a list of all students
         students = Student.all
         render json: students
     end
 
-    def grades
-        students = Student.order(grade: :desc)
+    def grades #4 Add a grades action in the StudentsController for handling that route
+        students = Student.order(grade: :desc) #4 In the grades action, return JSON data representing a list of all students, ordered by grade from highest to lowest. 
         render json: students
       end
 
-    def highest_grade
-        student = Student.order(grade: :desc).first
+    def highest_grade #5 Add a highest_grade action in the StudentsController for handling this route
+        student = Student.order(grade: :desc).first #5 Return JSON data representing the one student with the highest grade. This should return just one student object, unlike the other two, which returned arrays. 
         render json: student
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'students', to: 'students#index'
-  get 'students/grades', to: 'students#grades'
-  get '/students/highest-grade', to: 'students#highest_grade'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'students', to: 'students#index' #1. generates a GET request to /students
+  get 'students/grades', to: 'students#grades' #4 generate a GET request to /students/grades
+  get '/students/highest-grade', to: 'students#highest_grade' #5 generates a GET request to students/highest_grade
+  
 end
+
+#2 Generate a StudentsController with an index action for handling the route; 
+#2 Run: rails g controller Students --no-test-framework
+#2 Go to newly created file students_controller
